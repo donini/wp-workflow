c3994b03b0bad41ed0c231c5a04aec378ba1f517
William Oliveira <w.oliveira542@gmail.com>
2015-12-18 14:03:26 -0200

    Update backupMySQL.js

--- gulp/tasks/backupMySQL.js
+++ gulp/tasks/backupMySQL.js
@@ -2,7 +2,7 @@ module.exports = function(gulp, $, config, user, db, dest) {
 
   return function() {
     if(!user || !db || !dest) {
-      $.util.log($.util.colors.red('\n\n***************************************\nPlease, check if you typed the parameters.\n(eg: --user root --db revista --dest revista)\n'))
+      $.util.log($.util.colors.red('\n\n***************************************\nPlease, check if you typed the parameters.\n(eg: --user root --db database --dest backupdb)\n'))
     } else {
       gulp.src('./', { read : false })
         .pipe($.shell('mysqldump -u ' + user +' -p ' + db + ' > ../' + dest + '.sql'))
